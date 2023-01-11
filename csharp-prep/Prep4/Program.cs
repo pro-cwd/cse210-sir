5306f923cc1b0ead1d0e41969c30829ca4f86abf
codice Web & Design <proyectos@codicewd.com>
2023-01-11 14:38:23 -0500

    prep3, prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
-{
+{   
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int enter_number;
+        int maxnum = 0;
+        do
+        {
+            Console.Write("Enter number: ");
+            enter_number = int.Parse(Console.ReadLine());
+            numbers.Add(enter_number);
+        } while (enter_number != 0);
+        int nums;
+        int sum = numbers.Sum();
+        numbers.RemoveAt(numbers.Count - 1);
+        nums = numbers.Count;
+        // int div = int.Parse(nums);
+        double average = sum/nums;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        foreach (int num in numbers)
+        {
+            if (num > maxnum)
+            {
+                maxnum = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxnum}");
+        Console.WriteLine(String.Join(" ", numbers));
     }
 }
