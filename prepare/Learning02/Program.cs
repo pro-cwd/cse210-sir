a882f738196b1f849f3ceab45b4bfe9ab29483a3
codice Web & Design <proyectos@codicewd.com>
2023-01-18 14:09:45 -0500

    job and resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello Learning02 World!\n");
+
+        Resume resume = new Resume();
+        resume._name = "Alis in Chains";
+        resume.Display();
+
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Web Developer";
+        job1._startYear = 2023;
+        job1._endYear = 2030;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+        job2.Display();
+
     }
 }
