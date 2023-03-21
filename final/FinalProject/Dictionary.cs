31bec271e8b6ca79be7aeb8c7fa42dbebf305319
codice Web & Design <proyectos@codicewd.com>
2023-03-20 22:05:06 -0500

    milstone

---
+++ final/FinalProject/Dictionary.cs
@@ -0,0 +1,20 @@
+using system;
+
+public abstract class Dictionary
+{
+    protected string _word;
+    protected string _sentence1;
+    protected string _sentence2;
+    protected int _answer;
+    public int _grades;
+
+    public Dictionary(string word, string sentence1, string sentence2, int answer)
+    {
+        _word = word;
+        _sentence1 = sentence1;
+        _sentence2 = sentence2;
+        _answer = answer;
+    }
+    
+
+}
---
+++ final/FinalProject/menuOptions.cs
@@ -0,0 +1,15 @@
+public class MenuOption
+{
+    public MenuOption()
+    {
+
+    }
+
+    public void _MenuOption(list)
+    {
+        foreach (string i in choices)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
